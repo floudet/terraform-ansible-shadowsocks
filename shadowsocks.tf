a2f0e5ab8f15df2b423418694bd5c1e1e13b9bc1
Fabien Loudet <fabien.loudet@gmail.com>
2020-04-19 14:09:03 +0800

    Fix security group to allow ICMP echo

--- shadowsocks.tf
+++ shadowsocks.tf
@@ -51,9 +51,10 @@ resource "aws_security_group" "asg_shadowsocks" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = -1
-    to_port   = -1
+    from_port = 8
+    to_port   = 0
     protocol  = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
